2ab09592503d9dffb47082751a6c8017580a35f7
Saroj Poudel <sarox14@gmail.com>
2020-05-02 18:18:47 +0545

    form design donw

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,11 @@
         <v-row>
           <v-col cols="12" xs="12" sm="12" md="3" lg="3" xl="3">
             <v-img max-height="300" contain src="https://api.adorable.io/avatars/285/abott@adorable.png">
+              <template v-slot:placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-progress-circular indeterminate color="primary"></v-progress-circular>
+                </v-row>
+              </template>
             </v-img>
           </v-col>
           <v-col cols="12" xs="12" sm="12" md="9" lg="9" xl="9">
@@ -41,31 +46,35 @@
                 <p class="ma-0 pa-0 caption">NATIONALITY</p>
               </v-col>
               <v-col cols="20" xs="12" sm="4" md="4" lg="4" xl="4" class="py-0">
-                <p class="ma-0 pa-0 title">NEPALI<!-- <span class="overline">(NATIVE)</span> -->, ENGLISH
-                  <!-- <span class="overline">(INTERMEDIATE)</span> -->
+                <p class="ma-0 pa-0 title">NEPALI, ENGLISH
                 </p>
                 <p class="ma-0 pa-0 caption">LANGUAGE</p>
               </v-col>
             </v-row>
             <v-row no-gutters class="mb-6">
               <v-col cols="12" md="4" class="d-flex justify-space-between">
-                <v-btn icon outlined style="border:0px;" color="primary">
+                <v-btn icon outlined style="border:0px;" color="primary" router link href="https://www.facebook.com/"
+                  target="_blank">
                   <v-icon> fab fa-facebook
                   </v-icon>
                 </v-btn>
-                <v-btn icon outlined style="border:0px;" color="primary">
+                <v-btn icon outlined style="border:0px;" color="primary" router link href="https://www.twitter.com/"
+                  target="_blank">
                   <v-icon> fab fa-twitter
                   </v-icon>
                 </v-btn>
-                <v-btn icon outlined style="border:0px;" color="primary">
+                <v-btn icon outlined style="border:0px;" color="primary" router link href="https://www.linkedin.com/"
+                  target="_blank">
                   <v-icon> fab fa-linkedin
                   </v-icon>
                 </v-btn>
-                <v-btn icon outlined style="border:0px;" color="primary">
+                <v-btn icon outlined style="border:0px;" color="primary" router link href="https://www.slack.com/"
+                  target="_blank">
                   <v-icon> fab fa-slack
                   </v-icon>
                 </v-btn>
-                <v-btn icon outlined style="border:0px;" color="primary">
+                <v-btn icon outlined style="border:0px;" color="primary" router link href="https://www.dribbble.com/"
+                  target="_blank">
                   <v-icon> fab fa-dribbble
                   </v-icon>
                 </v-btn>
@@ -79,19 +88,19 @@
           </v-col>
           <v-col>
             <v-row no-gutters>
-              <v-col cols="12" lg="3">
+              <v-col cols="12" xs="6" sm="6" md="3" lg="3" xl="3">
                 <p class="ma-0 display-1 font-weight-black">100%</p>
                 <p class="ma-0">JOBS SUCCESS</p>
               </v-col>
-              <v-col cols="12" lg="3">
+              <v-col cols="12" xs="6" sm="6" md="3" lg="3" xl="3">
                 <p class="ma-0 display-1 font-weight-black">96%</p>
                 <p class="ma-0">ON BUDGET</p>
               </v-col>
-              <v-col cols="12" lg="3">
+              <v-col cols="12" xs="6" sm="6" md="3" lg="3" xl="3">
                 <p class="ma-0 display-1 font-weight-black">99%</p>
                 <p class="ma-0">ON TIME</p>
               </v-col>
-              <v-col cols="12" lg="3">
+              <v-col cols="12" xs="6" sm="6" md="3" lg="3" xl="3">
                 <p class="ma-0 display-1 font-weight-black">89%</p>
                 <p class="ma-0">REPEAT HIRE RATE</p>
               </v-col>
@@ -167,7 +176,7 @@
                     90%
                   </span>
                 </p>
-                <v-progress-linear class="mb-4" value="96"></v-progress-linear>
+                <v-progress-linear rounded class="mb-4" value="96"></v-progress-linear>
                 <p class="mb-2 d-flex justify-space-between">
                   <span>
                     CSS3
@@ -176,7 +185,7 @@
                     90%
                   </span>
                 </p>
-                <v-progress-linear class="mb-2" value="96"></v-progress-linear>
+                <v-progress-linear rounded class="mb-2" value="96"></v-progress-linear>
                 <p class="mb-2 d-flex justify-space-between">
                   <span>
                     PHP
@@ -185,7 +194,7 @@
                     90%
                   </span>
                 </p>
-                <v-progress-linear class="mb-2" value="96"></v-progress-linear>
+                <v-progress-linear rounded class="mb-2" value="96"></v-progress-linear>
                 <p class="mb-2 d-flex justify-space-between">
                   <span>
                     JAVASCRIPT
@@ -194,7 +203,7 @@
                     90%
                   </span>
                 </p>
-                <v-progress-linear value="96"></v-progress-linear>
+                <v-progress-linear rounded value="96"></v-progress-linear>
               </v-col>
               <v-col cols="12" lg="6" class="pt-0">
                 <p class="display-1 font-weight-black">DESIGN TOOLS</p>
@@ -206,7 +215,7 @@
                     90%
                   </span>
                 </p>
-                <v-progress-linear class="mb-4" value="96"></v-progress-linear>
+                <v-progress-linear rounded class="mb-4" value="96"></v-progress-linear>
                 <p class="mb-2 d-flex justify-space-between">
                   <span>
                     Illustrator
@@ -215,7 +224,7 @@
                     90%
                   </span>
                 </p>
-                <v-progress-linear class="mb-2" value="96"></v-progress-linear>
+                <v-progress-linear rounded class="mb-2" value="96"></v-progress-linear>
                 <p class="mb-2 d-flex justify-space-between">
                   <span>
                     Sketch
@@ -224,7 +233,7 @@
                     90%
                   </span>
                 </p>
-                <v-progress-linear class="mb-2" value="96"></v-progress-linear>
+                <v-progress-linear rounded="" class="mb-2" value="96"></v-progress-linear>
                 <p class="mb-2 d-flex justify-space-between">
                   <span>
                     MACAW
@@ -233,11 +242,41 @@
                     55%
                   </span>
                 </p>
-                <v-progress-linear value="55"></v-progress-linear>
+                <v-progress-linear rounded value="55"></v-progress-linear>
               </v-col>
             </v-row>
           </v-col>
         </v-row>
+
+        <v-row class="mb-6">
+          <v-col cols="12" lg="3">
+          </v-col>
+          <v-col>
+            <v-row no-gutters>
+              <v-col cols="12" class="pt-0">
+                <p class="display-1 font-weight-black">GET IN TOUCH</p>
+                <v-form>
+                  <v-row>
+                    <v-col cols="6" class="py-0">
+                      <v-text-field class="input" label="Full Name" solo></v-text-field>
+                    </v-col>
+                    <v-col cols="6" class="py-0">
+                      <v-text-field label="Email Address Name" solo></v-text-field>
+                    </v-col>
+                    <v-col cols="12" class="py-0">
+                      <v-text-field label="Subject" solo></v-text-field>
+                    </v-col>
+                    <v-col cols="12" class="py-0">
+                      <v-textarea label="Subject" solo></v-textarea>
+                    </v-col>
+                  </v-row>
+                  <v-btn color="primary" class="px-12">Submit</v-btn>
+                </v-form>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+
         <v-row no-gutters>
           <v-col cols="12" class="d-flex">
             <v-spacer></v-spacer>
@@ -274,7 +313,14 @@
         this.dark = !this.dark
         localStorage.setItem('darkmode', this.dark);
         this.$vuetify.theme.dark = this.dark
+        let a = window.matchMedia();
+        console.log(a);
       }
     }
   };
 </script>
+<style>
+div.v-input__slot{
+  border-bottom: 2px solid #FF5722 !important;
+}
+</style>
