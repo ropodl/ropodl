65c0f05cc82b96c2370d8496e7fab390d03cdfdb
Saroj Poudel <sarox14@gmail.com>
2020-05-03 09:57:07 +0545

    contactst

--- src/App.vue
+++ src/App.vue
@@ -316,7 +316,7 @@
 
         <v-row class="mb-6">
           <v-col cols="12" lg="3" class="d-flex">
-            <span class="title ml-auto font-weight-light">WORK EXPERIENCE</span>
+            <span class="title ml-auto font-weight-light">PORTFOLIO</span>
           </v-col>
           <v-col>
             <v-row>
@@ -359,6 +359,45 @@
             </v-row>
           </v-col>
         </v-row>
+
+        <v-row class="mb-6">
+          <v-col cols="12" lg="3" class="d-flex">
+            <span class="title ml-auto font-weight-light">CONTACT</span>
+          </v-col>
+          <v-col>
+            <v-row>
+              <v-col cols="12" lg="6" class="pt-0">
+                <p class="body-2 ma-0">ADDRESS</p>
+                <p class="display-1 font-weight-black ma-0">1355 Market Street, Suit 900</p>
+                <p class="ma-0 title">San Francisco, CA 94103</p>
+                <!-- <p class="subtitle ma-0">United Kingdom, London</p> -->
+              </v-col>
+              <v-col cols="12" lg="6" class="pt-0">
+                <p class="body-2 ma-0">PHONE NUMBER</p>
+                <p class="display-1 font-weight-black ma-0">N/A</p>
+                <!-- <p class="ma-0 title">BlissNiss</p> -->
+                <!-- <p class="subtitle ma-0">United Kingdom, London</p> -->
+              </v-col>
+              <v-col cols="12" lg="6">
+                <p class="body-2 ma-0">MOBILE NUMBER</p>
+                <p class="display-1 font-weight-black ma-0">9849808471</p>
+                <!-- <p class="ma-0 title">Unique Soft</p> -->
+                <!-- <p class="subtitle ma-0">United Kingdom, London</p> -->
+              </v-col>
+              <v-col cols="12" lg="6">
+                <p class="body-2 ma-0">EMAIL</p>
+                <p class="display-1 font-weight-black ma-0">
+                  <a href="mailto:sarox14@gmail.com" target="_blank" style="text-decoration:none;">
+                    sarox14@gmail.com
+                  </a>
+                </p>
+                <!-- <p class="ma-0 title">Somsom LLC</p> -->
+                <!-- <p class="subtitle ma-0">United Kingdom, London</p> -->
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+
 
         <v-row class="mb-6">
           <v-col cols="12" lg="3">
