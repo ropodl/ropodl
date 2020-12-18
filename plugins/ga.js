0506ed743192abcb6aeb877d187d5fdba6692c54
Saroj Poudel <sarox14@gmail.com>
2020-12-18 18:42:12 +0545

    analytics added

--- plugins/ga.js
+++ plugins/ga.js
@@ -1,9 +1,4 @@
-// <!-- Global site tag (gtag.js) - Google Analytics -->
-{/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-1KDFS20N0H"></script> */}
-{/* <script> */}
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-1KDFS20N0H');
-// </script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-1KDFS20N0H');
