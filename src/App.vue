2ef22015cb27191f5d1bb4ff7ee0c0595e82fa8c
ropodl <sarox14@gmail.com>
2023-04-06 10:15:51 +0545

    offline  banner added
    just because

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
+<script setup>
+import { defineAsyncComponent } from "vue";
+
+import { useNetwork } from "@vueuse/core";
+const { isOnline } = useNetwork();
+
+const OfflineBanner = defineAsyncComponent(() =>
+  import("@/components/layout/OfflineBanner.vue")
+);
+</script>
 <template>
   <v-app>
     <v-main>
-      <router-view></router-view>
+      <v-layout v-auto-animate>
+        <OfflineBanner v-if="!isOnline" />
+      </v-layout>
+      <router-view> </router-view>
     </v-main>
   </v-app>
 </template>
 <style>
 ::selection {
   color: white;
-  background: rgba(var(--v-theme-primary),1);
+  background: rgba(var(--v-theme-primary), 1);
 }
 </style>
---
+++ src/components/layout/OfflineBanner.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { mdiWifiStrengthAlertOutline } from "@mdi/js";
+</script>
+<template>
+  <v-container class="pb-0">
+    <v-banner
+      border
+      sticky
+      elevation="0"
+      lines="one"
+      rounded="xl"
+      :icon="mdiWifiStrengthAlertOutline"
+      text="You are currently not connect to the internet."
+    ></v-banner>
+  </v-container>
+</template>
+<style>
+.v-banner__content {
+  align-content: center !important;
+  display: grid !important;
+}
+</style>
