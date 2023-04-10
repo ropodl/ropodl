6900455e73cdc8432a94cd54d56780867f9ea1c6
ropodl <sarox14@gmail.com>
2023-04-10 16:44:00 +0545

    grid lock

--- src/App.vue
+++ src/App.vue
@@ -23,4 +23,12 @@ const OfflineBanner = defineAsyncComponent(() =>
   color: white;
   background: rgba(var(--v-theme-primary), 1);
 }
+@media (min-width: 1280px) and (min-width: 1920px) {
+  .v-container {
+    max-width: 1366px !important;
+  }
+  .v-container.v-container--fluid {
+    max-width: 100%;
+  }
+}
 </style>
