6c96e77aad80b300443a603846234e4a134dc487
ropodl <sarox14@gmail.com>
2023-04-30 04:38:44 +0545

    updatesw

--- src/main.js
+++ src/main.js
@@ -6,15 +6,6 @@ import { createApp } from "vue";
 
 // Plugins
 import { registerPlugins } from "@/plugins";
-import { registerSW } from "virtual:pwa-register";
-
-const updateSW = registerSW({
-  onOfflineReady() {
-    alert("We are now offline capable");
-  },
-});
-
-updateSW();
 
 const app = createApp(App);
 
