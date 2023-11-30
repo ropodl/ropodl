ce7ab8afda01498220ecc062e52c5cd3a93e8c16
ropodl <sarox14@gmail.com>
2023-11-30 07:06:03 +0545

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -119,8 +119,5 @@ export default defineNuxtConfig({
       clientId: process.env.NUXT_GITHUB_CLIENT_ID,
       clientSecret: process.env.NUXT_GITHUB_CLIENT_SECRET,
     },
-    public: {
-      authJs: {},
-    },
   },
 });
