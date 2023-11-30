2a9d5bf7a68f530c17cea6b23ace6a4f065a4fac
ropodl <sarox14@gmail.com>
2023-11-30 06:59:00 +0545

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -108,7 +108,7 @@ export default defineNuxtConfig({
   authJs: {
     guestRedirectTo: "/login", // where to redirect if the user is not authenticated
     authenticatedRedirectTo: "/loading/", // where to redirect if the user is authenticated
-    baseUrl: "https://ropodl.vercel.app/api/auth/", // The URL of your deployed app (used for origin Check in production)
+    baseUrl: process.env.BASE_URL, // The URL of your deployed app (used for origin Check in production)
     verifyClientOnEveryRequest: true, // whether to hit the /auth/session endpoint on every client request
   },
   runtimeConfig: {
--- server/middleware/1.auth.ts
+++ server/middleware/1.auth.ts
@@ -1,4 +1,15 @@
 export default defineEventHandler(async (event) => {
   const headers = await getRequestHeaders(event);
+  // setResponseHeaders(event, "Access-Control-Allow-Credentials:true");
   console.log(headers);
 });
+
+// res.setHeader('Access-Control-Allow-Credentials', true)
+// res.setHeader('Access-Control-Allow-Origin', '*')
+// // another common pattern
+// // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+// res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+// res.setHeader(
+//   'Access-Control-Allow-Headers',
+//   'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+// )
