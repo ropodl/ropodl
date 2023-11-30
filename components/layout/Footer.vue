039d8d6213ecdc9b11900b653db1c8663f89f78d
ropodl <sarox14@gmail.com>
2023-11-30 18:58:05 +0545

    going minimal

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,42 +1,12 @@
-<script setup>
-import { Icon } from "@iconify/vue";
-import { socials } from "@/utils/socials";
-</script>
 <template>
   <v-card
     border
     flat
-    class="pt-6 pb-16"
     rounded="0"
     style="border-bottom: 0; border-left: 0; border-right: 0"
   >
-    <v-card-text class="text-center">
-      <LazySharedLogo />
-    </v-card-text>
-    <v-card-text class="text-center">
-      <strong> Thanks for scrolling,</strong>&nbsp;that's all folks.
-      <ul class="d-flex justify-center list-style-none mt-8">
-        <li v-for="(social, i) in socials">
-          <v-hover v-slot="{ isHovering, props }">
-            <v-btn
-              rounded="lg"
-              height="40"
-              target="_blank"
-              v-bind="props"
-              size="small"
-              class="mr-3"
-              :variant="isHovering ? 'tonal' : 'text'"
-              :href="social['link']"
-              :aria-label="social['name']"
-              :color="isHovering ? 'primary' : 'white'"
-            >
-              <v-icon>
-                <Icon :icon="social['icon']" />
-              </v-icon>
-            </v-btn>
-          </v-hover>
-        </li>
-      </ul>
-    </v-card-text>
+    <v-card-title class="d-flex justify-center text-overline text-capitalize">
+      Copyright © {{ new Date().getFullYear() }} - Saroj Poudel
+    </v-card-title>
   </v-card>
 </template>
