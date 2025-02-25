913ae904c2a26ca5c52d8e7deaf2d8d856c35769
ropodl <sarox14@gmail.com>
2025-02-25 12:08:43 +0000

    test

--- components/admin/shared/QuillEditor.vue
+++ components/admin/shared/QuillEditor.vue
@@ -1,25 +1,59 @@
-<script setup lang="ts">
-import { QuillEditor } from "@vueup/vue-quill";
-import "@vueup/vue-quill/dist/vue-quill.snow.css";
-
+<script setup>
 const content = defineModel("content");
+
+defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+});
 </script>
 <template>
-  <QuillEditor
-    theme="snow"
-    toolbar="full"
-    v-model:content="content"
-    style="min-height: 500px"
-  ></QuillEditor>
+  <ClientOnly fallback="Loading editor...">
+    <lazy-common-shared-field-label>{{ label }}</lazy-common-shared-field-label>
+    <v-card border flat color="transparent" class="mb-4">
+      <QuillEditor
+        v-model:content="content"
+        content-type="html"
+        theme="snow"
+        toolbar="full"
+        style="min-height: 500px"
+      />
+    </v-card>
+  </ClientOnly>
 </template>
-
 <style lang="scss">
 .ql-toolbar.ql-snow {
-  border: 0;
-  border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+  border: 0 !important;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.3) !important;
+}
+.ql-editor {
+  height: 500px !important;
 }
 .ql-container.ql-snow {
-  border: 0;
-  border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+  border: 0 !important;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.3) !important;
 }
+.ql-snow-dark-border {
+  border: 1px solid rgba(255, 255, 255, 0.3) !important;
+}
+.ql-snow-light-border {
+  border: 1px solid rgba(0, 0, 0, 0.3) !important;
+}
+
+// .ql-snow.ql-toolbar {
+//   button {
+//     &:hover {
+//       background-color: #4b5563;
+//     }
+//     svg {
+//       path.ql-stroke {
+//         stroke: white !important;
+//       }
+//       line.ql-stroke {
+//         stroke: white !important;
+//       }
+//     }
+//   }
+// }
 </style>
