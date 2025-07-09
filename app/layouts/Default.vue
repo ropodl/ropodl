68cd07620e5c3f7033f9648bbf13daeb50c2eade
ropodl <sarox14@gmail.com>
2025-07-10 01:28:45 +0545

    refactor: simplify layout structure by removing unused background pattern and adjusting padding

--- app/layouts/Default.vue
+++ app/layouts/Default.vue
@@ -1,33 +1,6 @@
-<script setup>
-const defaults = ref({
-  global: {
-    theme: "front",
-  },
-});
-
-const bgPattern = `<div class="position-fixed w-100 h-100" style="opacity: 0.05;mask-image: radial-gradient(circle, #fff, transparent);">
-      <svg aria-hidden="true" class="position-absolute w-100 h-100">
-        <defs>
-          <pattern
-            id=":Rem:"
-            width="64"
-            height="64"
-            patternUnits="userSpaceOnUse"
-            x="50%"
-            y="100%"
-          >
-            <path d="M0 128V.5H128" fill="none" stroke="currentColor"></path>
-          </pattern>
-        </defs>
-        <rect width="100%" height="100%" fill="url(#:Rem:)"></rect>
-      </svg>
-    </div>`;
-</script>
 <template>
   <lazy-layout-navbar />
-  <div v-html="bgPattern"></div>
-  <v-main class="pt-5">
+  <v-main>
     <slot />
   </v-main>
-  <lazy-layout-footer />
 </template>
