616d9e2471d2b4503e4ebfa4d428d2eff7904676
ropodl <sarox14@gmail.com>
2025-07-10 00:38:10 +0545

    style: enhance footer layout with container and improved styling

--- app/components/layout/Footer.vue
+++ app/components/layout/Footer.vue
@@ -1,7 +1,17 @@
 <template>
-  <v-card border="t" rounded="0">
-    <v-card-title class="d-flex justify-center text-overline text-capitalize">
-      Copyright © {{ new Date().getFullYear() }} - Saroj Poudel
-    </v-card-title>
-  </v-card>
+  <v-container class="py-0">
+    <v-card
+      border
+      rounded
+      class="border-b-0 rounded-b-0"
+      style="
+        background-color: rgba(var(--v-theme-surface), 0.7);
+        backdrop-filter: blur(8px);
+      "
+    >
+      <v-card-title class="d-flex justify-center text-overline text-capitalize">
+        Copyright © {{ new Date().getFullYear() }} - Saroj Poudel
+      </v-card-title>
+    </v-card>
+  </v-container>
 </template>
